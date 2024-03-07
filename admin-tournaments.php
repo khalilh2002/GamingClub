41bb8412d45759980940ce46d61c638d9ce440a1
khalilh2002 <khalil.houssein2002@gmail.com>
2024-03-08 00:23:23 +0100

    ok

--- admin-tournaments.php
+++ admin-tournaments.php
@@ -37,10 +37,43 @@
     ?> 
 </body>
 <form enctype="multipart/form-data" action="upload.php" method="POST">
-    label.
+    <div class="row">
+        <div class="col-6">
+            <div class="form-floating mb-3">
+                <input type="text" name="card_title" class="form-control" placeholder="card_title"  id="cardtitleid">
+                <label for="cardtitleid">title</label>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class=" mb-3">
+                <input type="hidden" name="MAX_FILE_SIZE" value="9999999999" />
+                <label for="image" class="">Image File</label>
+                <input name="image" type="file" />
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="mb-3">
+                <label for="card_text" class="form-label">Description</label>
+                <textarea class="form-control" name="card_text" id="cardtextid" rows="5"></textarea>
+            </div> 
+        </div>
+        <div class="col-12">
+            <div class="form-floating mb-3">
+                <input
+                    type="url"
+                    class="form-control"
+                    name="linkid"
+                    id="linkid"
+                    placeholder="url"
+                />
+                <label for="linkid">url</label>
+            </div>
+            
+        </div>
 
-    <input type="hidden" name="MAX_FILE_SIZE" value="9999999999" />
-    Send this file: <input name="image" type="file" />
-    <input type="submit" name="submit" value="Send File" />
+    </div>
+     <a type="submit" name="submit" value="Send File" class="btn btn-outline-danger">ADD</a>   
+    
+    
 </form>
 </html>
