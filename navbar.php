6dd40634f2cc2f59d30cad0ab109f01d0ee6f295
goalaphx <idlahcen.elmahdi@etu.uae.ac.ma>
2024-03-07 16:13:15 +0100

    fixed the navlink issue

--- navbar.php
+++ navbar.php
@@ -1,7 +1,4 @@
-
-    
-    
-<nav class="navbar navbar-expand-lg bg-dark  border-bottom border-body" data-bs-theme="dark">
+<nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
     <div class="container-fluid">
         <a href="#" class="navbar-brand">
             <img src="img/agario.png" alt="Gaming Club" width="30" height="24">
@@ -9,35 +6,29 @@
         </a>
         <div class="collapse navbar-collapse navbar-brand" id="navbar001">
             <ul class="navbar-nav mb-2 me-auto mb-lg-0 ">
-
                 <li class="nav-item">
-                    <a href="index" aria-current="page" class="nav-link active">Home</a>
+                    <a href="index.php" aria-current="page" class="nav-link <?php if(basename($_SERVER['PHP_SELF']) == 'index.php') echo 'active'; ?>">Home</a>
                 </li>
-
                 <li class="nav-item">
-                    <a href="news" aria-current="page" class="nav-link">News</a>
+                    <a href="news.php" aria-current="page" class="nav-link <?php if(basename($_SERVER['PHP_SELF']) == 'news.php') echo 'active'; ?>">News</a>
                 </li>
-
                 <li class="nav-item">
-                    <a href="tournaments" aria-current="page" class="nav-link">Tournaments</a>
-
-                <li class="nav-item">
-                    <a href="contact" aria-current="page" class="nav-link">Contact Us</a>
+                    <a href="tournaments.php" aria-current="page" class="nav-link <?php if(basename($_SERVER['PHP_SELF']) == 'tournaments.php') echo 'active'; ?>">Tournaments</a>
                 </li>
-                
+                <li class="nav-item">
+                    <a href="contact.php" aria-current="page" class="nav-link <?php if(basename($_SERVER['PHP_SELF']) == 'contact.php') echo 'active'; ?>">Contact Us</a>
+                </li>
             </ul>
         </div>
         <div class="navbar-brand drop dropdown">
-            
-                <a href="#" role="button" data-bs-toggle="dropdown" 
+            <a href="#" role="button" data-bs-toggle="dropdown" 
                 class="nav-link dropdown-toggle" aria-expanded="false">
                 User
-                </a>
-                <ul class="dropdown-menu">
-                    <li><a href="#" class="dropdown-item">Login</a></li>
-                    <li><a href="#" class="dropdown-item">Inscription</a></li>
-                </ul>
-            
+            </a>
+            <ul class="dropdown-menu">
+                <li><a href="#" class="dropdown-item">Login</a></li>
+                <li><a href="#" class="dropdown-item">Inscription</a></li>
+            </ul>
         </div>
         <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbar001">
             <i class="fa-solid fa-bars"></i>
