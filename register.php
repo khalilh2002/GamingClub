d1a6803ddf3230dd55c0c6e4b57e08348bb409a4
khalilh2002 <khalil.houssein2002@gmail.com>
2024-03-10 18:37:13 +0000

    update

--- register.php
+++ register.php
@@ -119,10 +119,10 @@
         	<input type="email" class="form-control" name="email" placeholder="Email" required="required">
         </div>
 		<div class="form-group">
-            <input type="password" class="form-control" name="password" placeholder="Password" required="required">
+            <input type="text" class="form-control" name="username" placeholder="Username" required="required">
         </div>
 		<div class="form-group">
-            <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password" required="required">
+            <input type="password" class="form-control" name="password" placeholder="Password" required="required">
         </div>        
         <div class="form-group">
         <div class="hint-text">Already have an account? <a href="#">Login here</a></div>
