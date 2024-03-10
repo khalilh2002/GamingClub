cd2ea213c2d81c68182ae73109ee6be5fc547cfc
goalaphx <idlahcen.elmahdi@etu.uae.ac.ma>
2024-03-10 22:11:10 +0100

    font of admin navbar

--- admin-navbar.php
+++ admin-navbar.php
@@ -23,7 +23,7 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
+    <div class="collapse navbar-collapse" id="navbarNav" style="font-family: Georgia, 'Times New Roman', Times, serif;">
       <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link " aria-current="page" href="./admin-tournaments.php">tournaments</a>
