7ab27402f80169bc8d99f9e70e262cf5af14cb86
goalaphx <idlahcen.elmahdi@etu.uae.ac.ma>
2024-03-14 12:44:34 +0100

    adding pfp on navbar complete

--- navbar.php
+++ navbar.php
@@ -6,7 +6,18 @@ if (!session_status() === PHP_SESSION_ACTIVE) {
 require_once "./connect.php"; // Include your database connection file here
 
 ?>
+<style>
+    .profile-picture-container {
+    height: 13%;
+    width: 13%;
+    border-radius: 50%; /* Makes it circular */
+    overflow: hidden; /* Ensures image stays within circular container */
+}
 
+.top_pfp {
+    border-radius: 50%;
+}
+</style>
 
 <nav class="navbar navbar-expand-lg bg-black border-bottom border-body" data-bs-theme="dark">
     <div class="container-fluid">
@@ -44,17 +55,21 @@ require_once "./connect.php"; // Include your database connection file here
                         if (strpos($profile_picture, './') !== 0) {
                             $profile_picture = './' . $profile_picture;
                         }
-                        echo '<img src="' . $profile_picture . '" alt="Profile Picture" width="28" height="30">';
+                        echo '<a href="profile.php">';
+                        echo '<img src="' . $profile_picture . '" alt="Profile Picture" class="top_pfp" width="32" height="32">';
+                        echo '</a>';
                     } else {
-                        echo '<img src="./img/profile.png" alt="Default Profile Picture" width="28" height="30">';
+                        echo '<a href="profile.php">';
+                        echo '<img src="./img/profile.png" id="top_pfp" alt="Default Profile Picture" width="28" height="30">';
+                        echo '</a>';
                     }
                 } else {
-                    echo '<img src="./img/profile.png" alt="Default Profile Picture" width="28" height="30">';
+                    echo '<a href="profile.php">';
+                    echo '<img src="./img/profile.png" id="top_pfp" alt="Default Profile Picture" width="28" height="30">';
+                    echo '</a>';
                 }
                 echo '<a href="profile.php" aria-current="page" class="nav-link">My Profile</a>';
                 echo '<a href="logout.php" class="dropdown-item">Log Out</a>';
-                
-                
             } else {
                 echo '<a href="login.php" class="dropdown-item">Login</a>';
                 echo '<a href="register.php" class="dropdown-item">Register</a>';
